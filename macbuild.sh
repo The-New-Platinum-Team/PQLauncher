44a45fc0809162b69f5fa424b3804aae5b8352b0
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2024-12-19 21:38:24 +0530

    ci

--- macbuild.sh
+++ macbuild.sh
@@ -1,2 +1,3 @@
 dotnet publish -r osx-arm64 --configuration Release -f net8.0 -p:UseAppHost=true --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
+mkdir -p mac-dist/PQLauncher.app/Contents/MacOS
 mv bin/Release/net8.0/osx-arm64/publish/* "mac-dist/PQLauncher.app/Contents/MacOS/"
