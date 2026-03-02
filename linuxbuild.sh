235c115ae8b439d2684c5f94c94a079bdd1fae62
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2026-03-02 00:40:17 +0000

    linux build script

---
+++ linuxbuild.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Build self-contained single-file for Linux x64
+DOTNET_CLI_TELEMETRY_OPTOUT=1 dotnet publish -r linux-x64 --configuration Release -f net8.0 -p:UseAppHost=true --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true
+
+mkdir -p linux-dist/PQLauncher
+mv bin/Release/net8.0/linux-x64/publish/* "linux-dist/PQLauncher/"
+mv "linux-dist/PQLauncher/PQLauncher" "linux-dist/PQLauncher/PQLauncher"
+rm "linux-dist/PQLauncher/PQLauncher.pdb"
+
+tar -czf linux-dist/PQLauncher-linux-x64.tar.gz -C linux-dist PQLauncher
